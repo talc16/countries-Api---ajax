05cfe73fd355ee6d7582abd1b8399a2de7520d7f
tal c <talc16@gmail.com>
2019-08-16 17:04:56 +0300

    final version

--- by currency/ajax.js
+++ by currency/ajax.js
@@ -1,14 +1,15 @@
 
-function getCountriesByCapital(capitalInput, callback) {
+
+function getCountriesByCurrencty(currencyInput, callback) {
     $.ajax({
-        url: `https://restcountries.eu/rest/v2/capital/${capitalInput}`,
+        url: `https://restcountries.eu/rest/v2/currency/${currencyInput}`,
         method: "GET",
         success: function (result, status, response) {
+            $("#resultView").html("")
             console.log(result,status, response)
             callback(result)
         },
         error: function (err) {
-            
             $("#resultView").html("<h1>No Data!</h1>")
             $(".cards").html("")
         }
---
+++ by currency/index.js
@@ -0,0 +1,40 @@
+
+
+$(function () {
+
+    $("#search").on("click", function () {
+        const cName = $("#countrySearch").val()
+        getCountriesByCurrencty(cName, (result) => {
+            $(".cards").html("")
+            
+            $("#resultView").html(getCountriesNames(result))
+        })
+    })
+})
+let currenciesString=""
+function getCountriesNames(countries) {
+    
+    countries.map((country) => {
+        currenciesString=""
+        const { name, capital,currencies, flag } = country;
+        
+        currencies.map((currency) => { 
+            
+            const { name } = currency;
+            currenciesString = `${currenciesString} ${name},`
+            console.log(currenciesString)
+        })
+        // console.log(`${name}`)
+        $(".cards").append(`<div class="col-md-3"> <div class="card"> 
+        <img class="card-img" src="${flag}" style="width:100%; height:300px">
+        <div class="card-body">
+            <h4 class="card-title">${name}</h4>
+            <p class="card-text">Capital - ${capital} <br> Currencies - ${currenciesString}</p>
+            </div></div></div>`
+        )
+    })
+}
+// getCountries((countries) => {
+//     $("#resultView").html(getCountriesNames(countries))
+// });
+
